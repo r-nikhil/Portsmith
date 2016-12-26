1bbf03a32179e14a609c5fde3de4c7e528e1ff1c
Nikhil. R <rnikhil96@outlook.com>
2016-12-26 15:07:39 +0530

    fix portopener

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import os, sys
 from portsmith.log import Log
+from portsmith.profiles import Profiles
 from portsmith.knocklsiten import knockListener
-
+from portsmith.PortOpener import PortOpener
 def checkConditions():
 	if(not os.geteuid() == 0):
 		print("run the server code as root")
@@ -11,13 +12,19 @@ def checkConditions():
 		print("generate profiles first ")
 		sys.exit()
 
+def handleFirewall(input, config):
+	portOpener = PortOpener(input)
+	portOpener.waitForKnocks()
+
+def knockHandler(output, profiles):
+	portOpener = PortOpener(output)
+	knockListener = KnockListener(logFile, profiles, portOpener)
 
 def main(argv):
-	logFile = Log('/var/log/kern.log')
-	knockListener = KnockListener(logFile)
-	 
+	logFile = Log('/var/log/kern.log') 
 	checkConditions()
 
+	knockHandler(os.fdopen(output, "w"), profiles)
 
 
 
